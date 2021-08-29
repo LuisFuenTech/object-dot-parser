e2372bc4c2dc6fbbad978f11a73555c30f527105
Luis Fuentes Auraad <55701977+LuisFuentesAu@users.noreply.github.com>
2021-08-29 17:04:24 -0500

    Test: Add test settings. Add test files

---
+++ tests/index.test.js
@@ -0,0 +1,56 @@
+const objectDotParser = require("../src");
+
+describe("Object Dot Parser", () => {
+  const data = {
+    "person.name": "Luis",
+    "person.age": 74,
+    "person.address.city": "Atlanta",
+    "person.address.street.name": "St. Joseph",
+    "person.address.street.number": "#34-46",
+    "person.position": "Software Engineer",
+    "social.instagram": "luisfuentech",
+    "social.twitter": "luisfuentech",
+    "social.github": "luisfuentech"
+  };
+  const expectedData = {
+    person: {
+      name: "Luis",
+      age: 74,
+      address: {
+        city: "Atlanta",
+        street: {
+          name: "St. Joseph",
+          number: "#34-46"
+        }
+      },
+      position: "Software Engineer"
+    },
+    social: {
+      instagram: "luisfuentech",
+      twitter: "luisfuentech",
+      github: "luisfuentech"
+    }
+  };
+
+  it("Should parses an object with dotted keys string", () => {
+    const objectParsed = objectDotParser(data);
+
+    expect(typeof objectParsed).toEqual("object");
+    expect(objectParsed).toMatchObject(expectedData);
+  });
+
+  it("Should parses an array of objects with dotted keys string", () => {
+    const objectParsed = objectDotParser([data]);
+
+    expect(typeof objectParsed).toEqual("object");
+    expect(objectParsed).toMatchObject([expectedData]);
+    expect(objectParsed).toBeInstanceOf(Array);
+  });
+
+  it("Should return undefined when the param is no object or array", () => {
+    const objectParsed = objectDotParser();
+    console.log({ objectParsed });
+
+    expect(objectParsed).toBeUndefined();
+  });
+});
